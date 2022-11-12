047785252261be90103511e3e46c37fea9637f08
gitpher <jun_chois@naver.com>
2022-11-12 21:58:50 +0900

    initial commit

---
+++ src/main/kotlin/com/gitpher/kotlin2spring/Kotlin2springApplication.kt
@@ -0,0 +1,11 @@
+package com.gitpher.kotlin2spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlin2springApplication
+
+fun main(args: Array<String>) {
+    runApplication<Kotlin2springApplication>(*args)
+}
---
+++ src/test/kotlin/com/gitpher/kotlin2spring/Kotlin2springApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gitpher.kotlin2spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kotlin2springApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
